adc9af3839f36261b356527e966a4e4902b231d5
Anthony Lam <al3101@nyu.edu>
2018-05-17 03:16:13 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
-
+class CashRegister 
+  
+  attr_accessor :discount, :total 
+  
+  def initialize (discount = nil)  
+    @total = 0
+    @discount = discount 
+  end 
+    
+  # def total 
+  #   self.total 
+  # end 
+  
+  def add_item(title, price, quantity=1) 
+    self.total += (price * quantity) 
+  end 
+  
+  def apply_discount
+    if self.discount 
+      self.total = (self.total * self.discount/100) + self.total
+    end 
+  end
+end 
