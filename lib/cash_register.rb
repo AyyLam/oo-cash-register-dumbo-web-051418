dfa35c3fc557dbefe231fca874d64851e58f7254
Anthony Lam <al3101@nyu.edu>
2018-05-17 03:26:42 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,8 +18,9 @@ class CashRegister
   def apply_discount
     if self.discount 
       self.total -= self.total * self.discount/100
+      return 
     else 
-      
+      return "There is no discount to apply."
     end 
   end
   
